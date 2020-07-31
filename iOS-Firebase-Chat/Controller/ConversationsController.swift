02a62e3c83a099e944049159fa58d9438199811b
Yulia Popova <yyulia103@gmail.com>
2020-07-31 20:07:12 +0300

    configure navigation bar

--- iOS-Firebase-Chat/Controller/ConversationsController.swift
+++ iOS-Firebase-Chat/Controller/ConversationsController.swift
@@ -6,4 +6,46 @@
 //  Copyright © 2020 Yulia. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    @objc func showProfile() {
+
+    }
+    
+    // MARK: - Helpers
+    
+
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .systemIndigo
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.title = "Messages"
+        
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: UIBarButtonItem.Style.plain, target: self, action: #selector(showProfile))
+    }
+}
