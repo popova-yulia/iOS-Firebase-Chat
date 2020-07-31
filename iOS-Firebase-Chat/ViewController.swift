93d050c72a8ee77e58d0812ba9948a84866fe144
Yulia Popova <yyulia103@gmail.com>
2020-07-31 17:56:37 +0300

    Initial commit

---
+++ iOS-Firebase-Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Firebase-Chat
+//
+//  Created by Y u l i a on 31.07.2020.
+//  Copyright © 2020 Yulia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
